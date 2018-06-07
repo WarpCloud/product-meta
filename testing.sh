47f5419a5a2f41e658fa912b74759da85add2241
ggg <gggg@gggdeMacBook-Pro.local>
2018-06-07 15:00:55 +0800

    WARP-22796 json check test;add display_name for sophon
    add print

--- testing.sh
+++ testing.sh
@@ -5,6 +5,7 @@ set -e
 
 FILEPATH=$(cd ${0%/*} && echo $PWD/${0##*/})
 PROJROOT=$(cd $(dirname $FILEPATH) && pwd)
+export PROJROOT=$PROJROOT
 
 # Validate products/componets/applications/resources
 chmod +x $PROJROOT/tests/validate_products.sh
@@ -17,3 +18,4 @@ pip3 install -r $PROJROOT/tests/py-requirements.txt \
 chmod +x $PROJROOT/tests/validate_instance_images.py
 python3 $PROJROOT/tests/flex_version.py
 python3 $PROJROOT/tests/validate_instance_images.py
+python3 $PROJROOT/tests/validate_component_value.py
---
+++ tests/validate_component_value.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+import os
+import json
+import codecs
+
+
+def read_file(path):
+    files = os.listdir(path)
+    file_dict = dict()
+    for file in files:
+        file_name = path + '/' + file;
+        if os.path.isdir(file_name):
+            temp_dict = file_dict.copy()
+            temp_dict.update(read_file(path + '/' + file))
+            file_dict = temp_dict
+        elif file == 'default.json':
+            f = codecs.open(file_name, 'r', 'utf-8')
+            iter_f = iter(f)
+            str = ''
+            for line in iter_f:
+                str += line
+            file_dict[file_name] = json.loads(str)
+    return file_dict
+
+
+if __name__ == '__main__':
+    path = os.getenv('PROJROOT')
+    path = path + '/components'
+    files = read_file(path)
+    for item in files.items():
+        file_name = item[0]
+        template = item[1].get('templates')
+        print("checking file {}".format(file_name))
+        if template is None:
+            continue
+        for configs in template:
+            for object in configs.get('configs'):
+                assert object.get('value_type') != None
+                assert object.get('value') != None
+                assert object.get('name') != None
+                assert object.get('display_name') != None
+                assert object.get('description') != None
