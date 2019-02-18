06aed9eb60e76b0aa6f9cc6c11a6e780833b9cfd
Xin Xu <xin.xu@transwarp.io>
2019-02-18 17:58:21 +0800

    WARP-30234: remove invalid components and products for TDC-2.0

---
+++ tests/validate_etc.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+import yaml
+import os
+
+
+class OckleFile(object):
+    def __init__(self, file_dir):
+        settings_file = os.path.join(file_dir, 'settings.yml')
+        assert os.path.isfile(settings_file)
+        self.settings = yaml.load(open(settings_file, encoding='utf-8'))
+
+    def test_service_description(self, validate_assets):
+        for chart_name, d_svcs in self.settings.get('service_description', dict()).items():
+            for d_svc in d_svcs:
+                assert 'port' in d_svc
+                assert 'select_name' in d_svc
+                if 'resource_name' in d_svc:
+                    assert d_svc['resource_name'] in validate_assets
+
+if __name__ == '__main__':
+    path = os.getenv('PROJROOT')
+
+    file_path = path + '/etc/ockle'
+    assert os.path.isdir(file_path)
+
+    assets_path = path + '/assets'
+    assert os.path.isdir(assets_path)
+    assets_name = set(os.listdir(assets_path))
+
+    ockle_file = OckleFile(file_dir=file_path)
+    ockle_file.test_service_description(validate_assets=assets_name)
