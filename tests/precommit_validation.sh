b3c2d4fa95d54a0911049a92860fe524e254b2a2
Xiaming Chen <xiaming.chen@transwarp.io>
2019-06-10 15:45:01 +0800

    WARP-34008: move releases_meta.yaml into InfraTools/transwarp-continuous-delivery

--- tests/precommit_validation.sh
+++ tests/precommit_validation.sh
@@ -4,18 +4,22 @@ FILEPATH=$(cd ${0%/*} && echo $PWD/${0##*/})
 PROJROOT=$(cd $(dirname $FILEPATH)/.. && pwd)
 export PROJROOT=$PROJROOT
 
-verminator validate $PROJROOT/instances
+wget --header "PRIVATE-TOKEN: _CzK6kpWi2jkyT4BjBZ-" \
+"http://172.16.1.41:10080/api/v4/projects/InfraTools%2Ftranswarp-continuous-delivery/repository/files/script%2Fcommon%2Fmetainfo%2FTDC%2Ftdc-2.0%2Freleases%2Freleases_meta.yaml/raw?ref=master" \
+-O "/tmp/releases_meta.yaml"
+verminator validate --releasemeta /tmp/releases_meta.yaml $PROJROOT/instances
+
 CHANGED=`git status -s | wc -l`
 if [[ $CHANGED -gt 0 ]]; then
-echo "------ Git status ------"
-git status -s
-echo "------ Git diff ------"
-git diff > /tmp/ver_validation.diff
-cat /tmp/ver_validation.diff
-echo "Multiple files failed verminator validation!"
-echo "Please run: verminator validate /path/to/product-meta/instances"
-exit 1
+  echo "------ Git status ------"
+  git status -s
+  echo "------ Git diff ------"
+  git diff > /tmp/ver_validation.diff
+  cat /tmp/ver_validation.diff
+  echo "Multiple files failed verminator validation!"
+  echo "Please run: verminator validate /path/to/product-meta/instances"
+  exit 1
 else
-echo "SUCCESS"
-exit 0
+  echo "SUCCESS"
+  exit 0
 fi
